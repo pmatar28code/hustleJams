1b9295960ab3dcf2d2d0f1d149ff81d434b9f37d
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-11-21 01:06:23 -0600

    count down timer , image , track name and artist working on current workout from stored, added text view for counter  next left make it work when app in background with shared preferences and system time
    count down timer , image , track name and artist working on current workout from stored left make it work when app in background with shared preferences and system time.
    added textview for counter

--- app/src/main/java/com/example/hustlejams/fragments/CurrentWorkoutFromStored.kt
+++ app/src/main/java/com/example/hustlejams/fragments/CurrentWorkoutFromStored.kt
@@ -1,27 +1,29 @@
 package com.example.hustlejams.fragments
 
 import android.os.Bundle
-import android.os.Handler
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.hustlejams.MainActivity
 import com.example.hustlejams.R
 import com.example.hustlejams.Repository
 import com.example.hustlejams.database.WorkoutClass
 import com.example.hustlejams.databinding.FragmentCurrentWorkoutFromStoredBinding
-import com.example.hustlejams.networking.networkCalls.CurrentlyPlayingTrackNetwork
-import com.example.hustlejams.networking.networkCalls.GetCurrentPlaylistsNetwork
-import com.example.hustlejams.networking.networkClasses.CurrentlyPlayingTrack
+import com.example.hustlejams.networking.networkCalls.TrackDetailsNetwork
 import com.example.hustlejams.networking.networkClasses.GetPlaylistSpecific
 import com.google.gson.Gson
-import com.spotify.android.appremote.api.SpotifyAppRemote
-import com.spotify.protocol.types.PlayerState
 import com.spotify.protocol.types.Track
 import com.squareup.picasso.Picasso
-import java.util.*
+import java.util.concurrent.TimeUnit
+
 
 class CurrentWorkoutFromStored: Fragment(R.layout.fragment_current_workout_from_stored) {
+
+    var startTimeInMIlis:Int = 0
+    val listOfTracksIdsInPlaylist = mutableListOf<String>()
+    val listOfTrackDurations = mutableListOf<Int>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -43,43 +45,73 @@ class CurrentWorkoutFromStored: Fragment(R.layout.fragment_current_workout_from_
         }
         Log.e("PLAYLISTID FROM STRING INSDE WORKOUT CLASS","${playlistSpecificClass?.id}")
 
-        val playlistImagesList = playlistSpecificClass?.images
-        val selectedPlaylistImage = playlistImagesList?.get(0).toString()
-        val modifiedSelectedPlaylistImage = removeFromImageUrl(selectedPlaylistImage)
-        Log.e("PLAYLISY IMAGE",modifiedSelectedPlaylistImage)
-
-        Picasso.get().load(modifiedSelectedPlaylistImage).into(binding.backgroundImagePlaylistImage)
-
-        if (playlistSpecificClass != null) {
-            Repository.newlyCratedPlaylistId = playlistSpecificClass.id.toString()
+        val listOfTracksFromPlaylistSpecificClass = playlistSpecificClass?.tracks?.items
+        //listOfTracksIdsInPlaylist = mutableListOf<String>()
+        if (listOfTracksFromPlaylistSpecificClass != null) {
+            for(track in listOfTracksFromPlaylistSpecificClass){
+                listOfTracksIdsInPlaylist.add(track?.track?.id?:"")
+            }
         }
 
-        val activity = activity as MainActivity
+
+        //enterfun duration
+        getDurationFromFromNetworkUsingListOfIds {
+            Log.e("START TIME IN MILIS","$it")
 
 
+            val playlistImagesList = playlistSpecificClass?.images
+            val selectedPlaylistImage = playlistImagesList?.get(0).toString()
+            val modifiedSelectedPlaylistImage = removeFromImageUrl(selectedPlaylistImage)
+            Log.e("PLAYLISY IMAGE",modifiedSelectedPlaylistImage)
 
-        var playListSize = playlistSpecificClass?.tracks?.items?.size
+            Picasso.get().load(modifiedSelectedPlaylistImage).into(binding.backgroundImagePlaylistImage)
 
-        var counter =0
-
-
-
-        activity.playCurrentWorkoutPlaylist {
-            Repository.mSpotify?.playerApi?.subscribeToPlayerState()?.setEventCallback { playerState ->
-                val track: Track? = playerState.track
-                binding.currentWorkoutNowPlayingTrack.text = track?.name.toString()
-                //Log.e("TRACK IMAGE URI:","${track?.imageUri}")
-                Picasso.get().load(removeFromTrackUriInSetEventCallback(track?.imageUri.toString())).into(binding.backgroundImagePlaylistImage)
-
-
+            if (playlistSpecificClass != null) {
+                Repository.newlyCratedPlaylistId = playlistSpecificClass.id.toString()
             }
 
-            //Repository.mSpotify?.playerApi?.subscribeToPlayerState()?.setEventCallback {
-               // Log.e("EVENT CALLBACK PLAYER STATE,", it.track.name)
-               // binding.currentWorkoutNowPlayingTrack.text = it.track.name.toString()
+            val activity = activity as MainActivity
 
-            //}
+
+
+            var playListSize = playlistSpecificClass?.tracks?.items?.size
+
+            var counter =0
+
+
+
+            activity.playCurrentWorkoutPlaylist {
+                startCountdownTimer(binding)
+                Repository.mSpotify?.playerApi?.subscribeToPlayerState()?.setEventCallback { playerState ->
+                    val track: Track? = playerState.track
+                    binding.currentWorkoutNowPlayingTrack.text = track?.name.toString()
+                    //Log.e("TRACK IMAGE URI:","${track?.imageUri}")
+                    Picasso.get().load(removeFromTrackUriInSetEventCallback(track?.imageUri.toString())).into(binding.backgroundImagePlaylistImage)
+
+
+
+                }
+
+                //Repository.mSpotify?.playerApi?.subscribeToPlayerState()?.setEventCallback {
+                // Log.e("EVENT CALLBACK PLAYER STATE,", it.track.name)
+                // binding.currentWorkoutNowPlayingTrack.text = it.track.name.toString()
+
+                //}
+            }
         }
+
+
+
+        //val theTrackList = listOfTracksFromPlaylistSpecificClass[0].track.durationMs
+
+
+       // for(item in listOfTracksFromPlaylistSpecificClass!!){
+            //startTimeInMIlis = startTimeInMIlis.plus(item?.track?.durationMs?.toLong()?:0)
+           // Log.e("TRACKS DURATIONS:","${item?.track}")
+        //}
+
+
+
 
 /*
         activity.playCurrentWorkoutPlaylist(){
@@ -161,4 +193,46 @@ class CurrentWorkoutFromStored: Fragment(R.layout.fragment_current_workout_from_
     }
 
  */
+    private fun startCountdownTimer(binding: FragmentCurrentWorkoutFromStoredBinding) {
+    object : CountDownTimer(startTimeInMIlis.toLong(), 1000) {
+
+        override fun onTick(millisUntilFinished: Long) {
+            val minutes = String.format(
+                "%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                       TimeUnit.MINUTES.toSeconds(
+                            TimeUnit.MILLISECONDS.toMinutes(
+                                millisUntilFinished
+                            )
+                        ))
+            binding.mTextViewCountDown.text = minutes
+            when(binding.mTextViewCountDown.text){
+               "00:00" -> {
+                   Repository.mSpotify?.playerApi?.pause()
+               }else -> {Log.e("WHEN IN ONTICK","${binding.mTextViewCountDown.text}")}
+            }
+        }
+
+        override fun onFinish() {
+           Toast.makeText(requireContext(),"Workout has ended, as well as playlist",Toast.LENGTH_LONG).show()
+        }
+    }.start()
+    }
+
+    private fun getDurationFromFromNetworkUsingListOfIds(callback: (Int) -> Unit) {
+        val listSize = listOfTracksIdsInPlaylist.size
+        var counter = 0
+        for (id in listOfTracksIdsInPlaylist) {
+            ++counter
+            Repository.trackIdForTime = id
+            TrackDetailsNetwork.getTrackDetails {
+                startTimeInMIlis += it.duration_ms ?: 0
+                if(counter == listSize){
+                    callback(startTimeInMIlis)
+                }
+            }
+        }
+
+    }
 }
